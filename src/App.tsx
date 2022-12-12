8a7808b07c91af7fe587f05bd45c00facf3ceeea
mmmarco9 <marcoo_cs@hotmail.com>
2022-12-12 18:19:24 -0300

    Install e configuration paths with craco

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div style={{ display: 'flex', alignItems: "center", justifyContent: "center" }}>
+      <h1>Hello Word</h1>
+    </div>
+  )
+}
+
+export default App;
